8b5c5c15a0ca9252bd20a8fc1b039ff5f08de850
yeonghyeonSeok <syh4834@naver.com>
2019-10-11 16:21:19 +0900

    initial

---
+++ app/src/main/java/com/example/homework_1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.homework_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.style.UnderlineSpan
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var textName: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
